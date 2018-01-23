6fa2ebfc91126ad52b8ed977ca4c5049672f9fb4
alextarkhov <agtuks@gmail.com>
2018-01-23 05:51:01 +0200

    Update Middleware and composer

--- src/Middleware.php
+++ src/Middleware.php
@@ -6,25 +6,29 @@ use Symfony\Component\Serializer\Encoder\XmlEncoder;
 
 class Middleware
 {
-    const HEADER = 'Content-Type';
-    const MIME   = 'text/xml';
-    const OPTION = 'xml';
+    const HEADER_CONTENT_TYPE = 'Content-Type';
+    const MIME_TYPE_XML = 'text/xml';
+    const OPTION_XML = 'xml';
 
     public static function xml()
     {
         return function (callable $handler) {
             return function (RequestInterface $request, array $options) use ($handler) {
-                if (!empty($options[self::OPTION])) {
-                    $root = key($options[self::OPTION]);
-                    $data = current($options[self::OPTION]);
-                    if ($root && $data) {
-                        $encoder = new XmlEncoder($root);
-                        $body = $encoder->encode($data, XmlEncoder::FORMAT);
-                        unset($options[self::OPTION]);
-                        $request = $request->withHeader(self::HEADER, self::MIME)
-                            ->withBody(\GuzzleHttp\Psr7\stream_for($body));
-                    }
+                if (empty($options[self::OPTION_XML])) {
+                    return $handler($request, $options);
                 }
+
+                $root = key($options[self::OPTION_XML]);
+                $data = current($options[self::OPTION_XML]);
+                if (!$root || !$data) {
+                    return $handler($request, $options);
+                }
+
+                $encoder = new XmlEncoder($root);
+                $body = $encoder->encode($data, XmlEncoder::FORMAT);
+                unset($options[self::OPTION_XML]);
+                $request = $request->withHeader(self::HEADER_CONTENT_TYPE, self::MIME_TYPE_XML)
+                    ->withBody(\GuzzleHttp\Psr7\stream_for($body));
 
                 return $handler($request, $options);
             };
