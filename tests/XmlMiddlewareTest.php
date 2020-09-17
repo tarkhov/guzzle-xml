4ebae946b5c3bbef5417eef4475ace608975a398
tarkhov <agtuks@gmail.com>
2020-09-17 07:02:16 +0300

    Basic tests

---
+++ tests/XmlMiddlewareTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace GuzzleXml\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class XmlMiddlewareTest extends TestCase
+{
+    public function testRequest()
+    {
+        $this->assertTrue(true);
+    }
+}
